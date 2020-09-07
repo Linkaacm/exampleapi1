30a3c4a50da9ee4cc9afcf1eb2a1c816f1a5ac49
Armando Cruz Mendoza <acruzm@cecoban.org.mx>
2020-09-07 14:16:59 -0500

    Endpoint "health" para probar liveness probe de Kubernetes

---
+++ WebApplication1/Controllers/HealthController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var random = new Random();
+            var value = random.Next(0, 4);
+            if (value == 0)
+            {
+                return BadRequest($"Bad request ({value})");
+            }
+
+            return Ok($"Ok request! ({value})");
+
+        }
+    }
+}
